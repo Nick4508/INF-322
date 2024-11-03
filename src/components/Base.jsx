6bb85a50d4680e69580db6317e38211b8076f354
pipeeeh1 <felipe14789632@gmail.com>
2024-11-02 23:29:05 -0300

    Added link to logo, fixed size

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../stylesheets/Base/index.scss';
 import logo from '../assets/marca-color.png';
 
 const Base = () => {
     return (
       <div className="Base">
-        <img src={logo} alt="Logo" className="Base__logo" />
+        <Link to='/' className='Base__logolink'>
+          <img src={logo} alt="Logo" className="Base__logo" />
+        </Link>
       </div>
       
     );
