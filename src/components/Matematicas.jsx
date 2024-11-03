a4e247d4a974c5bccb52fce8dcabb7c357e8b0f4
pipeeeh1 <felipe14789632@gmail.com>
2024-11-02 23:07:31 -0300

    Added link to matematicas component

--- src/components/Matematicas.jsx
+++ src/components/Matematicas.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../stylesheets/Matematicas/Card.scss';
 import logo from '../assets/intro_matematicas.png';
 
 const Matematicas = () => {
+
   return (
-    <div className="card">
-        <div className="card-header">
-            <span className="header-text">MATEMATICA</span>
-            <img src={logo} alt="Logo" className="Matematicas__logo" />
+      <div className="card">
+          <div className="card-header">
+              <span className="header-text">MATEMATICA</span>
+              <Link to="/curso" className="card">
+              <img src={logo} alt="Logo" className="Matematicas__logo" />
+              </Link>
+          </div>
+        <div className="card-content">
+        <Link to="/curso" className="card">
+          <p className="course-title">
+            (202402)(MAT024) MATEMATICAS IV
+          </p>
+          <p className="parallels">
+            Paralelos: 200/201/202/203/204/205/206
+          </p>
+        </Link>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: '13%' }}></div>
+          </div>
+          <p className="progress-text">13% completado</p>
         </div>
-      <div className="card-content">
-        <p className="course-title">
-          (202402)(MAT024) MATEMATICAS IV
-        </p>
-        <p className="parallels">
-          Paralelos: 200/201/202/203/204/205/206
-        </p>
-        <div className="progress-bar">
-          <div className="progress" style={{ width: '13%' }}></div>
-        </div>
-        <p className="progress-text">13% completado</p>
       </div>
-    </div>
   );
 };
 
